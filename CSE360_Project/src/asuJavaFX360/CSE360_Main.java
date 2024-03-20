84a380a5dc0505bc22c4ce1332accd2de2c9941e
NicholasMAbate <116034915+NicholasMAbate@users.noreply.github.com>
2024-03-19 23:31:34 -0700

    Add files via upload
    This is the original upload of the main file. This will be known as version 1 and is subject to change. Every time you update the main file please update the date and the version on the top of the file before uploading the edit back onto github. If you make a new file follow the main files documentation style at the top and include a brief description of the file at the top of the file and in the commit changes (right here!).

---
+++ CSE360_Project/src/asuJavaFX360/CSE360_Main.java
@@ -0,0 +1,55 @@
+/* 
+ * ASU Spring 2024 CSE 360 11057
+ * Authors: Haroon Radmard, Nicholas Abate, Aiden Felix, Jackson Silvey, Chirag Jagadish
+ * Version: 1.0
+ * Original Version: March 19, 2024
+ * Last Updated: March 19, 2024
+ * 
+ * 1. Introduction
+ * The following is the implementation of an office automation system for a pediatric doctor's office. The system is 
+ * designed to be able to do the following. Provide, update, and store patient records. Establish communication 
+ * between doctor and patient via a messaging terminal. And provide medication prescriptions. 
+ * 	1.1 Clinic 49
+ *   The following software system is designed for the fictional pediatric doctor's office of Clinic 49. This software is
+ *   an academic exercise.
+ *  1.2 Patient Experience
+ *   TO BE ADDED 
+ *  1.3 Medical Professional Experience 
+ * 	 TO BE ADDED
+ *  
+ * 2. Detailed Design
+ *  2.1 Interface Design
+ *    2.1.1 GUI Design
+ *     TO BE ADDED
+ *    2.1.2 Web Design
+ *     TO BE ADDED 
+ *    2.1.3 Phone Application Design
+ *     TO BE ADDED 
+ *  2.2 Patient 
+ *   TO BE ADDED
+ *  2.3 Medical Professional
+ *   TO BE ADDED
+ *  2.4 Messaging Platform
+ *   TO BE ADDED
+ *  2.5 Patient Sign-Up
+ *   TO BE ADDED 
+ *  2.6 Patient Record Database
+ *   TO BE ADDED 
+ *   
+ * 3. Further Notes
+ * 	3.1  Github 
+ *   TO BE ADDED 
+ *  
+ *  
+ *  
+*/
+
+
+
+
+
+
+package asuJavaFX360;
+
+
+
